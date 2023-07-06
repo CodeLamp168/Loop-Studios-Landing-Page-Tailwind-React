8eff73a9b78b21d6cfb26219f6eb21de696686fe
Caleb D. Sim <dylansim168@gmail.com>
2023-07-06 12:58:45 +0800

    Added Responsiveness and Design

--- src/assets/Info.jsx
+++ src/assets/Info.jsx
@@ -4,14 +4,14 @@ export default function Info(){
         <section className="border-4 border-indigo-500">
             <div className="relative flex flex-col my-8 mx-2 xl:flex-row lg:my-16 justify-center items-center">
                 <div className="max-w-max">
-                    <img className="hidden xl:block w-auto" src="./imagelist/desktop/image-interactive.jpg" alt="404" />
+                    <img className="hidden relative xl:left-24 xl:block w-auto" src="./imagelist/desktop/image-interactive.jpg" alt="404" />
                     <img className="block xl:hidden"src="./imagelist/mobile/image-interactive.jpg" alt="404" />
 
                 </div>
 
-                <div className="max-w-xl  p-2 text-center md:max-w-2xl md:text-left">
-                    <h2 className="text-3xl md:text-5xl">Lorem ipsum dolor sit amet.</h2>
-                    <p className="text-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat expedita sint officia recusandae libero est debitis consequuntur voluptate consectetur dolor!</p>
+                <div className="mt-4 info-text  lg:bg-white max-w-xl  p-2 text-center md:max-w-2xl xl:text-left lg:relative xl:top-28 xl:right-24 xl:p-10">
+                    <h2 className="text-4xl md:text-5xl mb-2 ">THE LEADER IN INTERACTIVE VR</h2>
+                    <p className="text-1xl text-gray-500">Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
                 </div>
             </div>
         </section>
