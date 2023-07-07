89943e70b364c005d2647bbe942b73088ae2f94e
Caleb D. Sim <dylansim168@gmail.com>
2023-07-07 10:34:48 +0800

    Cleaning Up Gallery Responsiveness and Design

--- src/assets/Gallery.jsx
+++ src/assets/Gallery.jsx
@@ -3,53 +3,63 @@ export default function Gallery(){
     function GalleryDivs(){
 
         return(
-            <div className="gallery-image grid grid-rows-2 md:grid-cols-4 gap-4 p-4 place-content-center">
-                <div className="relative  max-w-xl">
-                    <img className="md:hidden" src="./imagelist/mobile/image-curiosity.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-curiosity.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3 0 max-w-0 uppercase tracking-widest">The Curiosity</p>
-                </div>
+            <div className="gallery-image grid grid-rows-2 md:grid-cols-4 gap-4 p-4 place-content-center ">
+          
 
-                <div className="relative  max-w-xl">
+                <div className="relative  max-w-xl ">
                     <img className="md:hidden" src="./imagelist/mobile/image-deep-earth.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-deep-earth.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">The Deep Earth</p>
-               </div>
-
-               <div className="relative  max-w-xl">
-                    <img className="md:hidden" src="./imagelist/mobile/image-fisheye.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-fisheye.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">The Fisheye</p>
-                    </div>
-               <div className="relative  max-w-xl">
-                    <img className="md:hidden" src="./imagelist/mobile/image-from-above.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-from-above.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">From Above</p>
-                </div>
-
-                <div className="relative  max-w-xl">
-                    <img className="md:hidden" src="./imagelist/mobile/image-grid.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-grid.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">The Grid</p>
+                    <img src="./imagelist/desktop/image-deep-earth.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5  max-w-0 uppercase tracking-widest">Deep Earth</p>
                </div>
 
                <div className="relative  max-w-xl">
                     <img className="md:hidden" src="./imagelist/mobile/image-night-arcade.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-night-arcade.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3 max-w-0 uppercase tracking-widest">Night Arcade</p>
+                    <img src="./imagelist/desktop/image-night-arcade.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5 max-w-0 uppercase tracking-widest">Night Arcade</p>
                </div>
+
+               <div className="relative  max-w-xl" >
+                    <img className="md:hidden" src="./imagelist/mobile/image-soccer-team.jpg" alt="404"/>
+                    <img src="./imagelist/desktop/image-soccer-team.jpg" alt="404" className="hidden md:block filter brightness-75 " />
+                    <p className="text-2xl lg:text-3xl absolute  bottom-3 left-5 md:bottom-6 left-5  w-44 uppercase tracking-widest">Soccer Team VR</p>
+               </div>
+
+               
+               <div className="relative  max-w-xl">
+                    <img className="md:hidden" src="./imagelist/mobile/image-grid.jpg" alt="404"/>
+                    <img src="./imagelist/desktop/image-grid.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5  max-w-0 uppercase tracking-widest">The Grid</p>
+               </div>
+
+               <div className="relative  max-w-xl">
+                    <img className="md:hidden" src="./imagelist/mobile/image-from-above.jpg" alt="404"/>
+                    <img src="./imagelist/desktop/image-from-above.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5  max-w-1 uppercase tracking-widest">From Up Above VR</p>
+                </div>
+         
+
+             
 
                <div className="relative  max-w-xl">
                     <img className="md:hidden" src="./imagelist/mobile/image-pocket-borealis.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-pocket-borealis.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">Pocket Borealis</p>
+                    <img src="./imagelist/desktop/image-pocket-borealis.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5  max-w-0 uppercase tracking-widest">Pocket Borealis</p>
                </div>
 
                <div className="relative  max-w-xl">
-                    <img className="md:hidden" src="./imagelist/mobile/image-soccer-team.jpg" alt="404"/>
-                    <img src="./imagelist/desktop/image-soccer-team.jpg" alt="404" className="hidden md:block" />
-                    <p className="text-2xl md:text-3xl absolute bottom-2 left-3  max-w-0 uppercase tracking-widest">Soccer Team</p>
+                    <img className="md:hidden" src="./imagelist/mobile/image-curiosity.jpg" alt="404"/>
+                    <img src="./imagelist/desktop/image-curiosity.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5 0 max-w-0 uppercase tracking-widest">The Curiosity</p>
+                </div>
+
+                
+               <div className="relative  max-w-xl">
+                    <img className="md:hidden" src="./imagelist/mobile/image-fisheye.jpg" alt="404"/>
+                    <img src="./imagelist/desktop/image-fisheye.jpg" alt="404" className="hidden md:block filter brightness-75" />
+                    <p className="text-2xl lg:text-3xl absolute bottom-3 left-5 md:bottom-6 left-5  max-w-0 uppercase tracking-widest">The Fisheye</p>
                </div>
+
+     
             </div>
         )
     }
